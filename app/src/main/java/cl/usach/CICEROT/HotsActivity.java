94067e61f27480b3d1653d6852e8e2682505d941
iorellana18 <ian.orellana@usach.cl>
2016-11-06 18:47:50 -0300

    list in fragment Hots

--- app/src/main/java/cl/usach/CICEROT/HotsActivity.java
+++ app/src/main/java/cl/usach/CICEROT/HotsActivity.java
@@ -6,11 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HotsActivity extends Fragment {
-
+    ListView list;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_hots, container, false);
+
+        View view = inflater.inflate(R.layout.activity_hots, container, false);
+        list =(ListView)view.findViewById(R.id.listNotices);
+
+        //EDITED Code
+        ArrayAdapter adapter =ArrayAdapter.createFromResource(getActivity(),
+                R.array.Noticias, android.R.layout.simple_list_item_1);
+
+        list.setAdapter(adapter);
+        return view;
     }
 }
