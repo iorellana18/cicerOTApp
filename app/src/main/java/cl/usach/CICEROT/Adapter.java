c813725156255075c065599d0390675dcb1e8ac2
iorellana18 <ian.orellana@usach.cl>
2016-11-06 20:54:30 -0300

    Set icons of toolbar to Primary color when it selected

--- app/src/main/java/cl/usach/CICEROT/Adapter.java
+++ app/src/main/java/cl/usach/CICEROT/Adapter.java
@@ -1,14 +1,17 @@
 package cl.usach.CICEROT;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 
@@ -36,8 +39,10 @@ public class Adapter extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_person_black_48dp));
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.blue_person));
         tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_collections_black_48dp));
         tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_whatshot_black_48dp));
         tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_email_black_48dp));
@@ -45,18 +50,48 @@ public class Adapter extends AppCompatActivity {
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final TabPagerAdapter adapter = new TabPagerAdapter
+
                 (getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+
+                //tab.setIcon(R.drawable.fire);---->Cambia por otro icono
+                switch (tab.getPosition()){
+                    case 0:
+                        tab.setIcon(R.drawable.blue_person);
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.blue_collector);
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.blue_hots);
+                        break;
+                    case 3:
+                        tab.setIcon(R.drawable.blue_mail);
+                        break;
+                }
                 viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                switch (tab.getPosition()){
+                    case 0:
+                        tab.setIcon(R.mipmap.ic_person_black_48dp);
+                        break;
+                    case 1:
+                        tab.setIcon(R.mipmap.ic_collections_black_48dp);
+                        break;
+                    case 2:
+                        tab.setIcon(R.mipmap.ic_whatshot_black_48dp);
+                        break;
+                    case 3:
+                        tab.setIcon(R.mipmap.ic_email_black_48dp);
+                        break;
+                }
             }
 
             @Override
